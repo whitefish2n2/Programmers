fb9facfc6eb333ae0a7e4c4b742918e02831d751
whitefish2n2 <whitefish_@naver.com>
2024-03-27 19:26:39 +0900

    a

---
+++ 120923.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace programmers120923;
+using System;
+using System.Linq;
+public class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int index = 0;
+        int j = 0;
+        for(int i = -50;;i++)
+        {
+            answer[j%num] = i;
+            if(total == Enumerable.Sum(answer))
+            {
+                Array.Sort(answer);
+                return answer;
+            }
+            j++;
+        }
+    }
+}
